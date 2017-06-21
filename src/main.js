533896e22e28c32493c77b336d0247a94d0760a7
mbliok <mbliok@melontech.com>
2017-06-21 15:39:52 +0300

    Checkbox - emit event form checkbox component to parent movie filter

--- src/main.js
+++ src/main.js
@@ -34,24 +34,40 @@ new Vue({
             template:`<div id="movie-filter">
                         <h2>Filter results</h2>
                         <div class="filter-group">
-                             <check-filter v-for="genre in genres" v-bind:filterTitle="genre"></check-filter>
+                             <check-comp 
+                                v-for="genre in genres" 
+                                v-bind:filterTitle="genre" 
+                                v-on:check-emit-name="checkFilter">
+                             </check-comp>
                         </div>
                      </div>`,
-            components:{ // the checkbox component
-                'check-filter':{
-                    data(){
-                        return{
-                            checked:false
+            methods:{
+                 checkFilter(){
+                     console.log('check this')
+                 }
+            },
+                    components:{ // the checkbox component
+                        'check-comp':{
+                            data(){
+                                return{
+                                    checked:false
+                                }
+                            },
+                            props:['filterTitle'],
+                            // if checkbox is true we have to add active class and he depend of cheched
+                            template:`<div v-bind:class="{'check-filter':true, active:checked}" v-on:click="checkFilter">
+                                        <span class="checkbox"></span>
+                                        <span class="check-filter-title">{{filterTitle}}</span>
+                                    </div>`,
+                            // emit event form checkbox component to parent movie filter
+                            methods:{
+                                checkFilter(){
+                                    this.checked = !this.checked
+                                    this.$emit('check-emit-name')
+                                }
+                            }
                         }
-                    },
-                    props:['filterTitle'],
-                    // if checkbox is true we have to add active class and he depend of cheched
-                    template:`<div v-bind:class="{'check-filter':true, active:checked}" v-on:click="checked = !checked">
-                                <span class="checkbox"></span>
-                                <span class="check-filter-title">{{filterTitle}}</span>
-                            </div>`
-                }
-            }
+                    }
        } 
     }
 
